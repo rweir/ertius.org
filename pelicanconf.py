ecc26295486a39b103d6bd491b5fd54cd4f6c7c3
Rob Weir <rweir@ertius.org>
2014-03-07 20:08:53 +1100

    some stuff

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Rob Weir'
+SITENAME = u'ertius.org'
+SITEURL = ''
+
+TIMEZONE = 'Australia/Sydney'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+
+DEFAULT_PAGINATION = False
+GOOGLE_ANALYTICS = "UA-44690246-2"
+GITHUB_URL = 'http://github.com/rweir/ertius.org'
+DISQUS_SITENAME = "ertiusorg"
+TIMEZONE = 'Australia/Sydney'
+TYPOGRIFY = True
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
